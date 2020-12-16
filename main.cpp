241a030a06cc18e58dbeacc3afea24dbde01bdd4
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-12-16 15:04:42 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "putsphere.h"
+#include "putvoxel.h"
+#include "cutbox.h"
+#include "cutellipsoid.h"
+#include "cutsphere.h"
+#include "cutvoxel.h"
+#include "interpretador.h"
+
+using namespace std;
+
+int main(){
+
+    Sculptor *s1;
+
+    Interpretador parser;
+    std:: vector<FiguraGeometrica*> figs;
+
+    figs = parser.parse("ler.txt");
+
+    s1 = new Sculptor(parser.getDimx(), parser.getDimy(), parser.getDimz());
+cout<< figs.size() <<endl;
+    for(size_t i=0; i<figs.size(); i++){
+                figs[i]->draw(*s1);
+    }
+    s1->limpaVoxels();
+    s1->writeOFF((char*)"saida.OFF");
+    for(size_t i=0; i<figs.size(); i++){
+        delete figs[i];
+    }
+    delete s1;
+    cout<< "Deu certo" <<endl;
+    return 0;
+}
