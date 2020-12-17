baa647ccafe0a49f2bcbd8cf67a6e6dc7c61d0fd
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-12-16 22:31:50 -0300

    Update cutvoxel.h

--- cutvoxel.h
+++ cutvoxel.h
@@ -3,20 +3,44 @@
 #include <figurageometrica.h>
 #include <sculptor.h>
 
-/**
- * @brief The cutvoxel class exclui uma unidade de voxel determinada pelo usuário
- * @param x determina a localização do voxel na coordenada x
- * @param y determina a localização do voxel na coordenada y
- * @param z determina a localização do voxel na coordenada z
- */
+    /**
+     * @brief The cutvoxel class exclui uma unidade de voxel determinada pelo usuário.
+     */
 
 class cutvoxel: public FiguraGeometrica{
 protected:
-    int x, y, z;
+
+    /**
+     * @brief x determina a localização do voxel na coordenada x.
+     */
+        int x;
+
+    /**
+     * @brief y determina a localização do voxel na coordenada y.
+     */
+        int y;
+
+    /**
+     * @brief z determina a localização do voxel na coordenada z.
+     */
+        int z;
+
 public:
     cutvoxel(int _x, int _y, int _z);
+
+    /**
+     * @brief ~cutvoxel é o destrutor da função <b>cutvoxel</b>.
+     */
+
     virtual ~cutvoxel();
+
+    /**
+     * @brief draw É o responsável por desenhar a figura desejada.
+     * @brief t Ficará responsável por armazenar o objeto desenhado pelo draw.
+     */
+
     void draw(Sculptor &t);
 };
 
 #endif // CUTVOXEL_H
+
