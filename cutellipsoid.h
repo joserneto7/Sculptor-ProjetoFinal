14501b0dab11db423928af0e50372c29c5ad6949
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-12-16 22:30:04 -0300

    Update cutellipsoid.h

--- cutellipsoid.h
+++ cutellipsoid.h
@@ -4,22 +4,60 @@
 #include <sculptor.h>
 #include <cmath>
 
-/**
- * @brief The cutellipsoid class faz um corte de formato elipsoidal da figura
- * @param xcenter determina o centro da elipse no eixo x
- * @param xcenter determina o centro da elipse no eixo y
- * @param xcenter determina o centro da elipse no eixo z
- * @param rx determina o raio da elipse no eixo x
- * @param ry determina o raio da elipse no eixo y
- * @param rz determina o raio da elipse no eixo z
- */
+    /**
+     * @brief The cutellipsoid class fará um corte de formato elipsoidal na figura, a partir das coordenadas estabelecidas.
+     */
+
 class cutellipsoid: public FiguraGeometrica{
 protected:
-    int xcenter, ycenter, zcenter, rx, ry, rz;
+
+    /**
+     * @brief xcenter determina o centro da elipse que será cortada, na coordenada x.
+     */
+        int xcenter;
+
+    /**
+     * @brief ycenter determina o centro da elipse que será cortada, na coordenada y.
+     */
+        int ycenter;
+
+    /**
+     * @brief zcenter determina o centro da elipse que será cortada, na coordenada z
+     */
+        int zcenter;
+
+    /**
+     * @brief rx determina o raio da elipse no eixo x.
+     */
+        int rx;
+
+    /**
+     * @brief ry determina o raio da elipse no eixo y.
+     */
+        int ry;
+
+    /**
+     * @brief rz determina o raio da elipse no eixo z.
+     */
+        int rz;
+
+
 public:
     cutellipsoid(int _xcenter, int _ycenter, int _zcenter, int _rx, int _ry, int _rz);
+
+    /**
+     * @brief ~cutellipsoid é o destrutor da da função <b>cuteEllipsoid</b>.
+     */
+
     virtual ~cutellipsoid();
+
+    /**
+     * @brief draw É o responsável por desenhar a figura desejada.
+     * @brief t Ficará responsável por armazenar o objeto desenhado pelo draw.
+     */
+
     void draw(Sculptor &t);
 };
 
 #endif // CUTELLIPSOID_H
+
