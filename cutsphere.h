24460718532482f487ef34d61ed3d57f1c8558ff
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-12-16 22:30:52 -0300

    Update cutsphere.h

--- cutsphere.h
+++ cutsphere.h
@@ -4,19 +4,47 @@
 #include <sculptor.h>
 #include <cmath>
 
-/**
- * @brief The cutsphere class faz um corte de formato esférico na figura
- * @param xcenter determina o centro da esfera no eixo x
- * @param xcenter determina o centro da esfera no eixo y
- * @param xcenter determina o centro da esfera no eixo z
- */
+    /**
+     * @brief The cutsphere class faz um corte de formato esférico na figura.
+     */
 
 class cutsphere: public FiguraGeometrica{
 protected:
-    int xcenter, ycenter, zcenter, radius;
+
+    /**
+ * @brief xcenter determina o centro da esfera no eixo x.
+ */
+        int xcenter;
+
+    /**
+     * @brief ycenter determina o centro da esfera no eixo y.
+     */
+        int ycenter;
+
+    /**
+     * @brief zcenter determina o centro da esfera no eixo z.
+     */
+        int zcenter;
+
+    /**
+     * @brief radius determina o raio da esfera
+     */
+        int radius;
+
 public:
     cutsphere(int _xcenter, int _ycenter, int _zcenter, int _radius);
+
+    /**
+     * @brief ~cutsphere é o destrutor da função cutsphere
+     */
+
     virtual ~cutsphere();
+
+    /**
+     * @brief draw É o responsável por desenhar a figura desejada.
+     * @brief t Ficará responsável por armazenar o objeto desenhado pelo draw.
+     */
+
     void draw(Sculptor &t);
 };
 
