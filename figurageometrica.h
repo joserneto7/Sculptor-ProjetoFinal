2adcc680b2504ceda349c576e0c7170f590aa70b
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-12-16 22:32:32 -0300

    Update figurageometrica.h

--- figurageometrica.h
+++ figurageometrica.h
@@ -2,13 +2,25 @@
 #define FIGURAGEOMETRICA_H
 #include <sculptor.h>
 
-/**
- * @brief The FiguraGeometrica class indica a clase que abarca todas as figuras geométricas que poderão ser desenhadas.
- */
+    /**
+     * @brief The FiguraGeometrica class indica a clase que abarca todas as classes abstratas que, estando associadas a esta, por meio de herança, poderão ser desenhadas.
+     */
+
 class FiguraGeometrica{
 public:
     FiguraGeometrica();
+
+    /**
+     * @brief ~FiguraGeometrica é o destrutor da função <b>FiguraGeometrica</b>.
+     */
+
     virtual ~FiguraGeometrica();
+
+    /**
+     * @brief draw É o responsável por desenhar a figura desejada.
+     * @brief t Ficará responsável por armazenar o objeto desenhado pelo draw.
+     */
+
     virtual void draw(Sculptor &t)=0;
 };
 
