b2881848cc609d975b86fba16ca0cdba309db048
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-12-16 22:33:21 -0300

    Update interpretador.h

--- interpretador.h
+++ interpretador.h
@@ -5,33 +5,74 @@
 #include "figurageometrica.h"
 using namespace std;
 
-/**
- * @brief A classe Interpretador constroi o desenho
- */
+    /**
+     * @brief The Interpretador class irá decodificar o desenho que será criado.
+     */
 
 
 class Interpretador{
-    int dimx, dimy, dimz;
-    float r, g, b, a;
+
+    /**
+     * @brief dimx é a dimensão da figura no eixo x.
+     */
+    int dimx;
+
+    /**
+     * @brief dimy é a dimensão da figura no eixo y.
+     */
+    int dimy;
+
+    /**
+     * @brief dimz é a dimensão da figura no eixo z.
+     */
+    int dimz;
+
+    /**
+     * @brief r determina a cor <b>Vermelha</b> da figura.
+     */
+    float r;
+
+    /**
+     * @brief g determina a cor <b>Verde</b> da figura.
+     */
+    float g;
+
+    /**
+     * @brief b determina a cor <b>Azul</b> da figura.
+     */
+    float b;
+
+    /**
+     * @brief a determina a opacidade da cor escolhida.
+     */
+    float a;
+
+
 public:
 
     Interpretador();
 
     std::vector<FiguraGeometrica *>parse(std::string filename);
+
     /**
      * @brief getDimx
-     * @return - Retorna o tamanho da matriz no eixo x
+     * @return - Retorna o tamanho da matriz no eixo x.
      */
+
     int getDimx();
+
     /**
      * @brief getDimy
-     * @return - Retorna o tamanho da matriz no eixo y
+     * @return - Retorna o tamanho da matriz no eixo y.
      */
+
     int getDimy();
+
     /**
      * @brief getDimz
-     * @return - Retorna o tamanho da matriz no eixo z
+     * @return - Retorna o tamanho da matriz no eixo z.
      */
+
     int getDimz();
 
 };
