3433e8dc53aafefb72b93cecefd068aca85eff02
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-12-16 22:38:18 -0300

    Update voxel.h

--- voxel.h
+++ voxel.h
@@ -2,12 +2,13 @@
 #define VOXEL_H
 
 /**
- * @brief The Voxel struct Como já foi falado anteriormente suas funções
+ * @brief The Voxel struct Como já foi falado anteriormente suas funções.
  * */
 struct Voxel {
   float r,g,b; // Colors
   float a; // Transparency
   bool isOn; // Included or not
+
 };
 
 #endif // VOXEL_H
