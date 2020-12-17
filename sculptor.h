ba368c7f5f7dac7b2e18a52c072ce4d529ef2bba
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-12-16 22:37:42 -0300

    Update sculptor.h

--- sculptor.h
+++ sculptor.h
@@ -2,18 +2,25 @@
 #define SCULPTOR_H
 /**
  * @brief The Voxel struct
- * Armazena o valor escolhido para cada cor, como também para opacidade.
- * Determina se o voxel é ativo ou não
  */
 
 struct Voxel {
+
+  /**
+   * @brief r define a tonalidade vermelha da figura.
+   * @brief g define a tonalidade azul da figura.
+   * @brief b define a tonalidade azul da figura.
+   * @brief alpha Armazena o valor escolhido para cada cor, como também para opacidade.
+   * @brief isOn Determina se o voxel é ativo ou não.
+   */
+
   float r,g,b;
   float alpha;
   bool isOn;
 };
 
 /**
- * @brief A classe Sculptor
+ * @brief The Sculptor class torna-se responsável por desenvolver a figura.
  */
 
 class Sculptor{
@@ -25,9 +32,9 @@ public:
 
   /**
    * @brief Sculptor
-   * @param _nx será a dimensão da figura em x;
-   * @param _ny será a dimensão da figura em y;
-   * @param _nz será a dimensão da figura em z;
+   * @brief _nx será a dimensão da figura em x.
+   * @brief _ny será a dimensão da figura em y.
+   * @brief _nz será a dimensão da figura em z.
    */
 
   Sculptor(int _nx, int _ny, int _nz);
@@ -35,39 +42,39 @@ public:
 
   /**
    * @brief setColor Define a cor da figura
-   * @param r Determina, variando de 0 a 1, a tonalidade da cor red(vermelho) que será atribuida.
-   * @param g Determina, variando de 0 a 1, a tonalidade da cor green(verde) que será atribuida.
-   * @param b Determina, variando de 0 a 1, a tonalidade da cor blue(azul) que será atribuida.
-   * @param alpha Determina a opacidade da figura, variando também de 0 a 1.
+   * @brief r Determina, variando de 0 a 1, a tonalidade da cor red(vermelho) que será atribuida.
+   * @brief g Determina, variando de 0 a 1, a tonalidade da cor green(verde) que será atribuida.
+   * @brief b Determina, variando de 0 a 1, a tonalidade da cor blue(azul) que será atribuida.
+   * @brief alpha Determina a opacidade da figura, variando também de 0 a 1.
    */
 
   void setColor(float r, float g, float b, float alpha);
 
   /**
    * @brief putBox Cria um conjunto de voxels dentro da matriz 3d
-   * @param x0 valor inicial de coordenada x
-   * @param x1 valor final da coordenada x
-   * @param y0 valor inicial da coordenada y
-   * @param y1 valor final da coordenada y
-   * @param z0 valor inicial da coordenada z
-   * @param z1 valor final da coordenada z
+   * @brief x0 valor inicial de coordenada x.
+   * @brief x1 valor final da coordenada x.
+   * @brief y0 valor inicial da coordenada y.
+   * @brief y1 valor final da coordenada y.
+   * @brief z0 valor inicial da coordenada z.
+   * @brief z1 valor final da coordenada z.
    */
 
   void putVoxel(int x, int y, int z);
 
   /**
-   * @brief cutVoxel Corta uma unidade de voxel
-   * @param x Atribui o valor a coordenada x
-   * @param y Atribui o valor a coordenada y
-   * @param z Atribui o valor a coordenada z
+   * @brief cutVoxel Corta uma unidade de voxel.
+   * @brief x Atribui o valor a coordenada x.
+   * @brief y Atribui o valor a coordenada y.
+   * @brief z Atribui o valor a coordenada z.
    */
 
   void cutVoxel(int x, int y, int z);
   void limpaVoxels(void);
 
   /**
-   * @brief writeOFF
-   * @param filename Recebe o que foi indicado, abre esse arquivo e escreve dentro dele os vertices e faces
+   * @brief writeOFF escreve o aquivo que contém o executável da figura desenhada..
+   * @brief filename Recebe o que foi indicado, abre esse arquivo e escreve dentro dele os vertices e faces.
    */
 
   void writeOFF(char* filename);
