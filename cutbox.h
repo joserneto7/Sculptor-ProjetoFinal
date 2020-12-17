870ad1441059dcd522d38691fac4d08c6b198f54
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-12-16 22:29:13 -0300

    Update cutbox.h

--- cutbox.h
+++ cutbox.h
@@ -3,22 +3,60 @@
 #include <figurageometrica.h>
 #include <sculptor.h>
 
-/**
- * @brief The CutBox class Corta um conjunto de voxels em formato de Cubo
- * @param x0 indica o ínicio da localização do box na coordenada x
- * @param x1 indica o final da localização no box na coordenada x
- * @param y0 indica o ínicio da localização do box na coordenada y
- * @param y1 indica o final da localização no box na coordenada y
- * @param z0 indica o ínicio da localização do box na coordenada z
- * @param z1 indica o final da localização no box na coordenada z
- */
+    /**
+     * @brief The CutBox class Remove um conjunto de voxels em formato de Cubo.
+     */
+
 class CutBox: public FiguraGeometrica{
 protected:
-    int x0, x1, y0, y1, z0, z1;
+
+    /**
+     * @brief x0 indica a coordenada, em x, que delimita o cubo a ser retirado da figura.
+     */
+        int x0;
+
+    /**
+     * @brief x1 indica a coordenada final, em x, que limita até onde o cubo será retirado.
+     */
+        int x1;
+
+    /**
+     * @brief y0 indica a coordenada, em y, que delimita o cubo a ser retirado da figura.
+     */
+        int y0;
+
+    /**
+     * @brief y1 indica a coordenada final, em y, que limita até onde o cubo será retirado.
+     */
+        int y1;
+
+    /**
+     * @brief z0 indica a coordenada, em z, que delimita o cubo a ser retirado da figura.
+     */
+        int z0;
+
+    /**
+     * @brief z1 indica a coordenada final, em z, que limita até onde o cubo será retirado.
+     */
+        int z1;
+
 public:
     CutBox(int _x0, int _x1, int _y0, int _y1, int _z0, int _z1);
+
+    /**
+     * @brief ~CutBox é o destrutor da classe abstrata <b>CutBox</b>.
+     * @sa CutBox
+     */
+
     virtual ~CutBox();
+
+    /**
+     * @brief draw É o responsável por desenhar a figura desejada.
+     * @brief t Ficará responsável por armazenar o objeto desenhado pelo draw.
+     */
+
     void draw(Sculptor &t);
 };
 
 #endif // CUTBOX_H
+
